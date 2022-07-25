e1d282de538eec79111e0ec9ad882294912ae150
autoaim_jp <git@autoaim.jp>
2022-07-25 19:35:39 +0900

    update: xdevkit v0.7

--- setting/xdevkitSetting.js
+++ setting/xdevkitSetting.js
@@ -1,15 +1,30 @@
 /* /setting/xdevkitSetting.js */
 
 const setting = {}
-setting.url = {}
-setting.url.ERROR_PAGE = '/error'
-
 setting.session = {}
 setting.session.SESSION_ID = 'sid'
 setting.session.SESSION_COOKIE_SECURE = false
 setting.session.REDIS_PORT = 6379
 setting.session.REDIS_HOST = '127.0.0.1'
 setting.session.REDIS_DB = 2
+
+setting.api = {}
+setting.api.API_VERSION = 'v0.7'
+setting.api.STATE_L = 64
+setting.api.CODE_VERIFIER_L = 64
+setting.api.XLOGIN_RESPONSE_TYPE = 'code'
+setting.api.XLOGIN_CODE_CHALLENGE_METHOD = 'S256'
+setting.api.SCOPE = 'r:emailAddress,*r:userName,*r:serviceUserId'
+setting.api.FILTER_KEY_LIST = ['emailAddress', 'userName', 'serviceUserId']
+
+setting.url = {}
+setting.url.ERROR_PAGE = '/error'
+setting.url.XLOGIN_AUTHORIZATION_ENDPOINT = `/api/${setting.api.API_VERSION}/auth/connect`
+setting.url.XLOGIN_CODE_ENDPOINT = `/api/${setting.api.API_VERSION}/auth/code`
+setting.url.XLOGIN_USER_INFO_ENDPOINT = `/api/${setting.api.API_VERSION}/user/info`
+setting.url.XLOGIN_REDIRECT_URI = encodeURIComponent('/f/xlogin/callback')
+
+
 
 export const init = (env) => {
   setting.env = {}
